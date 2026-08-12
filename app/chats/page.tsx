50a0f3590ada4000baae4d1a76a6ee3f58df288b
prada021703-oss <prada021703@gmail.com>
2026-08-11 22:34:19 -0500

    Add app/chats/page.tsx

---
+++ app/chats/page.tsx
@@ -0,0 +1,9 @@
+import WhatsAppChatViewer from '@/components/WhatsAppChatViewer';
+
+export default function ChatsPage() {
+  return (
+    <div className="p-6 bg-zinc-950 min-h-screen">
+      <WhatsAppChatViewer />
+    </div>
+  );
+}
